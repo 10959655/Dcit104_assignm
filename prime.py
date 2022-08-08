8915ba05440209db95ff89caa1537001c724d621
10959655 <102669539+10959655@users.noreply.github.com>
2022-08-08 04:55:10 -0700

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,4 +1,14 @@
+#this was to fine if the numbers between the range is prime, so i replace 5 with each value in the interval.
+  num = 5
+  b = num/1
+  a = num % num
+  if b ==num and a==0:
+    print(num)
+    
+    
+    
 PrimeNumbers = [2,3,5,7,11]
 y = sum(PrimeNumbers)
 x = len(PrimeNumbers)
   print(y/x)
+#I listed the set of prime numbers between 1 and 12 in an array and i went ahesd to sum values in the array and count it as well and afterward, i found average.
