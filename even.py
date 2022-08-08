7a0172b8dc8d615dfef4abe57308feafe5401cd6
10959655 <102669539+10959655@users.noreply.github.com>
2022-08-08 04:47:54 -0700

    Update even.py

--- even.py
+++ even.py
@@ -3,3 +3,7 @@ y = sum(x)
 z = len(x)
 b = y/z
 print (b)
+
+
+#The 0 and 10 in the interval for the x values and the 2 is the increament. I then went ahead to add and cout them with sum and len method. Finding average, I divided the sum 
+#which is the  y by the number of values which is z and printed out as b.
