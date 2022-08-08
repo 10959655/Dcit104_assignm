16f9c4867ffaed351834fc3027cd23d86e7c022f
10959655 <102669539+10959655@users.noreply.github.com>
2022-08-08 05:12:12 -0700

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,14 +1,3 @@
-#this was to fine if the numbers between the range is prime, so i replace 5 with each value in the interval.
-  num = 5
-  b = num/1
-  a = num % num
-  if b ==num and a==0:
-    print(num)
-  else:
-    print("no")
-    
-    
-    
 PrimeNumbers = [2,3,5,7,11]
 y = sum(PrimeNumbers)
 x = len(PrimeNumbers)
